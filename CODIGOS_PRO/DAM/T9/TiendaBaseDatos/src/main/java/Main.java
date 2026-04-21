eef4949ac30699fdcb3f1a799835177aa1e8a8f3
Develop and System <developandsystem@gmail.com>
2026-04-21 19:58:37 +0200

    update

--- CODIGOS_PRO/DAM/T9/TiendaBaseDatos/src/main/java/Main.java
+++ CODIGOS_PRO/DAM/T9/TiendaBaseDatos/src/main/java/Main.java
@@ -1,12 +1,13 @@
-import java.sql.*;
+import controller.TiendaController;
+import database.DBConnection;
+import model.User;
+
+import java.sql.Connection;
 
 public class Main {
     public static void main(String[] args)  {
-        try {
-            Connection connection = DriverManager.getConnection("jdbc:mysql://127.0.0.1/tienda_thpw_dam","root","root");
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-
+        TiendaController controller = new TiendaController();
+        // controller.darAltaUsuario(new User("lucas","lucas@gmail.com",24000,1));
+        controller.obtenerListado();
     }
 }
